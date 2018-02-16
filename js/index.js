541fa7d721ad3f9ae568ef078f0485cc628ac695
Hana Xu <tug11775@temple.edu>
2018-02-16 10:11:24 -0500

    added email validation & popup text

--- js/index.js
+++ js/index.js
@@ -1,14 +1,25 @@
 $(document).ready(function(){
-
-var emailText = document.getElementById("emailText");
-var emailSubmitBtn = document.getElementById("emailSubmitBtn");
-
-var firebaseRef = firebase.database().ref("EmailList");
-
-function emailSubmitClick(){
 	
-	firebaseRef.push(emailText.value);
-	
-}	
+	var emailText = document.getElementById("emailText");
+	var emailSubmitBtn = document.getElementById("emailSubmitBtn");
+
+	var firebaseRef = firebase.database().ref("EmailList");		
+
+	emailSubmitClick = function(){	
+		document.getElementById("message").className= ""; //remove hidden class & show message	
 		
+		if(ValidateEmail(emailText.value)){
+			firebaseRef.push(emailText.value);			
+		}
+		else{
+			document.getElementById("message").innerHTML= "Invalid Email";
+			document.getElementById("message").style.color= "red";		
+		}
+	}
+	
+	function ValidateEmail(mail) {
+	    return (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail))
+	}
+	
 });
+

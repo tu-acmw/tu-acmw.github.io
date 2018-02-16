d4e30d6f56bb5d1e128041fa3655507463a0b9e4
Hana Xu <tug11775@temple.edu>
2018-02-16 10:58:41 -0500

    cleaned up

--- js/index.js
+++ js/index.js
@@ -2,23 +2,26 @@ $(document).ready(function(){
 	
 	var emailText = document.getElementById("emailText");
 	var emailSubmitBtn = document.getElementById("emailSubmitBtn");
-
+	var message = document.getElementById("message");
+	
 	var firebaseRef = firebase.database().ref("EmailList");		
 
 	emailSubmitClick = function(){	
-		document.getElementById("message").className= ""; //remove hidden class & show message	
+		message.className= ""; //remove hidden class & show message	
 		
 		if(ValidateEmail(emailText.value)){
-			firebaseRef.push(emailText.value);			
+			firebaseRef.push(emailText.value);
+			message.innerHTML= "Success!";
+			message.style.color= "#19c433";			
 		}
 		else{
-			document.getElementById("message").innerHTML= "Invalid Email";
-			document.getElementById("message").style.color= "red";		
+			message.innerHTML= "Invalid Email";
+			message.style.color= "red";		
 		}
 	}
 	
 	function ValidateEmail(mail) {
-	    return (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail))
+	    return (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) && mail.length <=254;
 	}
 	
 });

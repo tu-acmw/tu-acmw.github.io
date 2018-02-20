c959ca32381bef42de1bc37b717ee4fa9e262034
Hana Xu <tug11775@temple.edu>
2018-02-19 20:19:08 -0500

    added name input to sign up form

--- js/index.js
+++ js/index.js
@@ -1,18 +1,23 @@
 $(document).ready(function(){
 	
+	var nameText = document.getElementById("nameText");
 	var emailText = document.getElementById("emailText");
 	var emailSubmitBtn = document.getElementById("emailSubmitBtn");
 	var message = document.getElementById("message");
 	
-	var firebaseRef = firebase.database().ref("EmailList");		
+	var firebaseRef = firebase.database().ref();		
 
 	emailSubmitClick = function(){	
 		message.className= ""; //remove hidden class & show message	
-		
-		if(ValidateEmail(emailText.value)){
-			firebaseRef.push(emailText.value);
+
+		if(ValidateEmail(emailText.value) && nameText.value != ""){
+			firebaseRef.child(nameText.value).set({Email: emailText.value});
 			message.innerHTML= "Success!";
 			message.style.color= "#19c433";			
+		}
+		else if(nameText.value == ""){
+			message.innerHTML= "Please enter your name";
+			message.style.color= "red";	
 		}
 		else{
 			message.innerHTML= "Invalid Email";

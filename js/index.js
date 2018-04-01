5868a17e12ec58fdd4a1d695bcffcd4445b86d56
Hanaxu <hanaxu12@gmail.com>
2018-04-01 04:15:04 -0400

    added feedback faq & mailchimp for sign up

--- js/index.js
+++ js/index.js
@@ -3,26 +3,11 @@ $(document).ready(function(){
 	var firebaseRef = firebase.database().ref().child("Members");
 
 	emailSubmitClick = function(){
-		message.className= ""; //remove hidden class & show message
-
 		if(ValidateEmail(emailText.value) && ValidateName(firstName.value) && ValidateName(lastName.value)){
 			firebaseRef.child(firstName.value + " " + lastName.value).set({"First name": firstName.value, 
 													"Last name": lastName.value,
 													"Email" : emailText.value});
-													
-			message.innerHTML= "Success!";
-			message.style.color= "#19c433";
 		}
-		else if(firstName.value == '' || firstName.value.length >40
-				|| lastName.value == '' || lastName.value.length >40){
-			message.innerHTML= "Please enter your name";
-			message.style.color= "red";
-		}
-		else{
-			message.innerHTML= "Invalid Email";
-			message.style.color= "red";
-		}
-
 	}
 	
 	function ValidateEmail(mail) {
